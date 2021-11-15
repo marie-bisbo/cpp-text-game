348edc8b5c0edc2b8cf3bfa2d26618c734bdd884
marie-bisbo <marie@brainlabsdigital.com>
2021-11-15 08:44:36 +0000

    Create character based on choice

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 enum class CharacterType 
 {
@@ -6,9 +8,68 @@ enum class CharacterType
     Mage
 };
 
+struct Item
+{
+};
+
+struct Spell
+{
+};
+
+class Character
+{
+protected:
+    std::string m_Name;
+    std::vector<Item> m_Inventory;
+    Item m_Weapon;
+
+public:
+    Character();
+
+    Character(std::string name, std::vector<Item> inventory, Item weapon)
+        : m_Name(name), m_Inventory(inventory), m_Weapon(weapon)
+    {
+        m_Inventory.reserve(10);
+        m_Inventory.push_back(weapon);
+    }
+};
+
+class Fighter : public Character
+{
+public:
+    Fighter(std::string name, std::vector<Item> inventory, Item weapon) 
+        : Character(name, inventory, weapon)
+    {
+        std::cout << "I am a Fighter!" << std::endl;
+    }
+};
+
+class Mage : public Character
+{
+private:
+    std::vector<Spell> m_Spells;
+
+public:
+    Mage(std::string name, std::vector<Item> inventory, Item weapon, std::vector<Spell> spells)
+        : Character(name, inventory, weapon), m_Spells(spells)
+    {
+        std::cout << "I am a Mage!" << std::endl;
+    }
+};
+
+
+void ChoseCharacter();
 
 int main()
 {
+    ChoseCharacter();
+
+    return 0;
+}
+
+void ChoseCharacter()
+{
+    CharacterType chosenCharacter;
     bool hasChosenCharacter = false;
     while (!hasChosenCharacter)
     {
@@ -21,16 +82,21 @@ int main()
         {
             case 0:
                 std::cout << "You chose to be a Fighter" << std::endl;
+                chosenCharacter = CharacterType::Fighter;
                 hasChosenCharacter = true;
                 break;
             case 1:
                 std::cout << "You chose to be a Mage" << std::endl;
+                chosenCharacter = CharacterType::Mage;
                 hasChosenCharacter = true;
                 break;
             default:
                 std::cout << "You did not chose a valid character type" << std::endl;
         }
     }
-
-    return 0;
+    Character* character;
+    if (chosenCharacter == CharacterType::Fighter)
+        character = new Fighter("Bob", std::vector<Item>(), Item());
+    if (chosenCharacter == CharacterType::Mage)
+        character = new Mage("Alice", std::vector<Item>(), Item(), std::vector<Spell>());
 }
